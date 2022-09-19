e1d0327c57151cca3f311d88524e8c00d5670da1
muh001 <112939986+muh001@users.noreply.github.com>
2022-09-19 14:01:18 +0300

    Update jetbot_motors.py

--- jetbot_motors.py
+++ jetbot_motors.py
@@ -12,25 +12,41 @@ from std_msgs.msg import String
 def set_speed(motor_ID, value):
 	max_pwm = 115.0
 	speed = int(min(max(abs(value * max_pwm), 0), max_pwm))
+	a = b =0
+	
 
 	if motor_ID == 1:
 		motor = motor_left
+		a = 1
+		b = 0
 	elif motor_ID == 2:
 		motor = motor_right
+		a = 2
+		b = 3
 	else:
 		rospy.logerror('set_speed(%d, %f) -> invalid motor_ID=%d', motor_ID, value, motor_ID)
 		return
 	
 	motor.setSpeed(speed)
 
-	if value > 0:
+	if value < 0:
 		motor.run(Adafruit_MotorHAT.FORWARD)
-	else:
+		motor.MC._pwm.setPWM(a,0,0)
+		motor.MC._pwm.setPWM(b,0,speed*16)
+	elif value > 0:
 		motor.run(Adafruit_MotorHAT.BACKWARD)
+		motor.MC._pwm.setPWM(a,0,speed*16)
+		motor.MC._pwm.setPWM(b,0,0)
+	else:
+		motor.run(Adafruit_MotorHAT.RELEASE)
+		motor.MC._pwm.setPWM(a,0,0)
+		motor.MC._pwm.setPWM(b,0,0)
 
 
 # stops all motors
 def all_stop():
+	set_speed(motor_left_ID, 0.0)
+	set_speed(motor_right_ID, 0.0)
 	motor_left.setSpeed(0)
 	motor_right.setSpeed(0)
 
